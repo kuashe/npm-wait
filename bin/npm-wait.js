5c0b7d48a3d29fdd337b6708142ce5b5d0611219
Quentin Homareau <quentin.homareau@gmail.com>
2019-03-02 18:22:27 +0100

    Add support for http

--- bin/npm-wait.js
+++ bin/npm-wait.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
 
+const {get}    = require('http')
+
+const target = process.argv[2]
+
+function createRequest(){
+
+        let request = get( target , onResponse )
+        request.on('error' , () => {})
+
+        return request
+}
+
+function onResponse(response){
+
+    if(response.statusCode == 200 )
+    {
+        process.exit(1)
+    }
+  
+}
+
+
+setInterval( createRequest , 100)
